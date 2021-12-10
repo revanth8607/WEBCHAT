d1933307367155ccf91274770149cb71741b032e
Revanth sai gowrisetty <93648364+revanth8607@users.noreply.github.com>
2021-12-10 12:15:37 +0530

    Create AESEncryption.cs

---
+++ AESEncryption.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace WEBCHAT
+{
+    public class AESEncryption
+    {
+        public string EncryptText(string inputValue, string inputsalt)
+        {
+
+            string EncryptionKey = "REVANTH";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(inputValue);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    inputValue = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return inputValue;
+
+        }
+        public string DecryptText(string inputValue, string inputsalt)
+            {
+                string EncryptionKey = "REVANTH";
+                byte[] cipherBytes = Convert.FromBase64String(inputValue.Replace(" ", "+"));
+                using (Aes encryptor = Aes.Create())
+                {
+                    Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                    encryptor.Key = pdb.GetBytes(32);
+                    encryptor.IV = pdb.GetBytes(16);
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(cipherBytes, 0, cipherBytes.Length);
+                            cs.Close();
+                        }
+                        inputValue = Encoding.Unicode.GetString(ms.ToArray());
+                    }
+                }
+                return inputValue;
+            }
+           
+    }
+}
