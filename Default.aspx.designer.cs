6fa0f0cfa6959ccf42d7cfd14578e1a614af012c
Revanth sai gowrisetty <93648364+revanth8607@users.noreply.github.com>
2021-12-10 12:14:06 +0530

    Create Default.aspx.designer.cs

---
+++ Default.aspx.designer.cs
@@ -0,0 +1,45 @@
+
+namespace WEBCHAT
+{
+
+
+    public partial class _Default
+    {
+
+        /// <summary>
+        /// dlMessages control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList dlMessages;
+
+        /// <summary>
+        /// txtMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtMessage;
+
+        /// <summary>
+        /// btnSend control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSend;
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+    }
+}
